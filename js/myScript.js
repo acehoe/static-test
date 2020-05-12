ce1567a20908d36e871610c89079ed5e6719af88
weiwei100 <weiwei1076@gmail.com>
2020-05-13 00:10:15 +0800

    update div background and update pic url

--- js/myScript.js
+++ js/myScript.js
@@ -1,3 +1,3 @@
 function myFunction() {
-    document.getElementById("demoimg").src="http://localhost:9999/images/g_photographer.jpg";
+    document.getElementById("demoimg").src="http://13.231.229.207:8080/static/images/g_photographer.jpg";
 }
